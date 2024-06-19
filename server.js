2a0ac498954203f0031f43223a7f6b0c256008ba
guruteja2000 <rudraguruteja2000@gmail.com>
2024-06-19 16:40:59 +0530

    third commit

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const jsonServer = require("json-server")
 const path = require("path")
+const cors = require("cors")
 
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, "src", "db.json"))
+const router = jsonServer.router(path.join("./", "src", "db.json"))
 const middlewares = jsonServer.defaults()
 
+server.use(cors())
 server.use(middlewares)
 server.use(router)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3003
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`)
 })
